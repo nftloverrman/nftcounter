8e24f43f64bfa11c7e465099dc5e2835053ef9d3
nftloverrman <143797775+nftloverrman@users.noreply.github.com>
2023-12-15 23:21:46 +0330

    Update jdi.js

--- jdi.js
+++ jdi.js
@@ -51,7 +51,7 @@ const nextConfig = {
         permanent: true,
       },
       {        destination: '/lexicons/app-bsky-actor',
-        permanent: false,
+        permanent: true,
       },
     ]
   },
