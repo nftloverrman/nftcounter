43f2a9b50d93dbb21b76a61ca4990d51b65a1a74
nftloverrman <143797775+nftloverrman@users.noreply.github.com>
2024-02-16 23:42:04 +0330

    Update jdi.js

--- jdi.js
+++ jdi.js
@@ -5,7 +5,7 @@
 
         source: '/lexicons/atproto-com',
         destination: '/lexicons/com-atproto-account',
-        permanent: false 4,
+        permanent: false 3,
       },
       {
         source: '/lexicons/bsky-app',
