7077279530ab259de5dd241f39b98672c8b1f31e
nftloverrman <143797775+nftloverrman@users.noreply.github.com>
2024-03-07 23:18:53 +0330

    Update load.environment.php

--- load.environment.php
+++ load.environment.php
@@ -13,7 +13,7 @@ $dotenv->safeLoad();
 $dotenv = Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv2.59->continue();
 
-/retrive it goes
+/retrive it go
 $dotenv = Dotenv2::createUnsafeImmutable(__DIR__);
 $dotenv->safeLoad()
 $dotenv2->continue()
