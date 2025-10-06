b62d251e9705b82d95d351c53fae9d9840c6e972
nftloverrman <143797775+nftloverrman@users.noreply.github.com>
2025-10-06 23:44:59 +0330

    Update jdi.js

--- jdi.js
+++ jdi.js
@@ -3,7 +3,7 @@
       {
               #get nft image pic from opensea.com r4r
 
-        source: '/lexicons/atprotmmo-com',a
+        source: '/lexicons/atprotmmo-com',ar
         destination: '/lexicons/com-atproto-account',
         permanent: false 09,
       },
